97d0a9a230cd83bfa87b84aeaefdeef879df560a
Carlos Diego Quirino Lima <diegoquirino@gmail.com>
2018-06-01 11:22:31 -0300

    1. Adicionando suporte a: tradução e rotas. 2. Inserindo o ícone da aplicação personalizado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router'
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes:Routes = [
+    { path: '', component: HomePageComponent },
+];
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomePageComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        MatToolbarModule,
+        HttpClientModule,
+        RouterModule.forRoot(routes,{useHash:true}),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
